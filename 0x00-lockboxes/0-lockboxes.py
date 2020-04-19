1073cfbf6aacb90ced0624334bd115f44df8f969
alzheimeer <fogniebla@hotmail.com>
2020-04-19 00:59:16 -0500

    feature task

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,2 +1,29 @@
-!/usr/bin/python3
+#!/usr/bin/python3
+""" boxes """
 
+
+def canUnlockAll(boxes):
+
+    if(len(boxes) == 0):
+        return False
+    boxes[0].append(0)
+    boxes[0] = list(set(boxes[0]))
+    ff = boxes[0]
+    
+    if (len(boxes) == len(boxes[0])):
+        return True
+    
+
+    for x in boxes[0]: 
+        ff = ff + boxes[x]
+        ff = list(set(ff))
+        if (len(boxes) != len(boxes[0])):
+            boxes[0] = ff
+    
+    if (len(boxes) != len(boxes[0])):
+        boxes[0] = ff
+    
+    try:    
+        return (canUnlockAll(boxes))
+    except:
+        return False
