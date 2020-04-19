44d84a3ae7fa865e46044d48e11c8dbef6205b91
alzheimeer <fogniebla@hotmail.com>
2020-04-19 01:25:09 -0500

    fix boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,26 +4,24 @@
 
 def canUnlockAll(boxes):
 
-    if(len(boxes) == 0):
-        return False
+    if len(boxes) == 0:
+        return True
     boxes[0].append(0)
     boxes[0] = list(set(boxes[0]))
     ff = boxes[0]
-    
     if (len(boxes) == len(boxes[0])):
         return True
-    
 
-    for x in boxes[0]: 
+    for x in boxes[0]:
         ff = ff + boxes[x]
         ff = list(set(ff))
         if (len(boxes) != len(boxes[0])):
             boxes[0] = ff
-    
+
     if (len(boxes) != len(boxes[0])):
         boxes[0] = ff
-    
-    try:    
+
+    try:
         return (canUnlockAll(boxes))
     except:
         return False
