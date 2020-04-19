6ca9a3c57cef6ee2a0e2a9a9b34f306a229234a4
alzheimeer <fogniebla@hotmail.com>
2020-04-19 15:47:03 -0500

    fix without recursive

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-""" boxes """
-
 
 def canUnlockAll(boxes):
 
-    if not boxes:
-        return False
-    if len(boxes) is 0:
+    f = list(range(1, len(boxes)))
+    ff = []
+    a = True
+
+    while a:
+        for x in boxes[0]:
+            if x > len(boxes):
+                x = 0
+            if not boxes[x]:
+                boxes[0].remove(x)
+                ff.append(x)
+                ff = list(set(ff))
+            for y in boxes[x]:
+                if (x != None) and (y < len(boxes) and y != 0):
+                    boxes[0] = boxes[0] + boxes[x]
+                    boxes[0] = list(set(boxes[0]))
+                    for z in boxes[0]:
+                        if z > len(boxes):
+                            boxes[0].remove(z)
+                    boxes[x].clear()
+                    ff.append(x)
+                    ff = list(set(ff))
+                    boxes[0].remove(x)
+                else:
+                    boxes[x].remove(y)
+        if (len(boxes[0]) == 0):
+            a = False
+
+    if ff == f:
         return True
-    boxes[0].append(0)
-    boxes[0] = list(set(boxes[0]))
-    ff = boxes[0]
-    if (len(boxes) == len(boxes[0])):
-        return True
-
-    for x in boxes[0]:
-        ff = ff + boxes[x]
-        ff = list(set(ff))
-        if (len(boxes) != len(boxes[0])):
-            boxes[0] = ff
-
-    if (len(boxes) != len(boxes[0])):
-        boxes[0] = ff
-
-    try:
-        return (canUnlockAll(boxes))
-    except:
-        return False
+    return False
