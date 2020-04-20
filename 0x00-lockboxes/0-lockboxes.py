abb8929109702cf39cb8c9e8fd31e55b5b3f4afd
alzheimeer <fogniebla@hotmail.com>
2020-04-19 22:30:03 -0500

    fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,40 +8,20 @@ def canUnlockAll(boxes):
         True (bool): for success,
         False (bool): In case of failure.
     '''
-    if (type(boxes) is not list):
+    box = list(range(len(boxes)))
+    keys = [False]*len(boxes)
+    keys[0] = True
+    for k in boxes[0]:
+        if k in box:
+            keys[k] = True
+
+    for c in range(len(boxes)-2):
+        for i in range(1, len(boxes)):
+            if keys[i] is True:
+                for k in boxes[i]:
+                    if k in box:
+                        keys[k] = True
+
+    if False in keys:
         return False
-
-    if (len(boxes) == 0):
-        return False
-
-    f = list(range(1, len(boxes)))
-    ff = []
-    a = True
-    
-    while a:
-        for x in boxes[0]:
-            if x > len(boxes):
-                x = 0
-            if not boxes[x]:
-                boxes[0].remove(x)
-                ff.append(x)
-                ff = list(set(ff))
-            for y in boxes[x]:
-                if (x is not None) and (y < len(boxes) and y != 0):
-                    boxes[0] = boxes[0] + boxes[x]
-                    boxes[0] = list(set(boxes[0]))
-                    for z in boxes[0]:
-                        if z > len(boxes):
-                            boxes[0].remove(z)
-                    boxes[x].clear()
-                    ff.append(x)
-                    ff = list(set(ff))
-                    boxes[0].remove(x)
-                else:
-                    boxes[x].remove(y)
-        if (len(boxes[0]) == 0):
-            a = False
-
-    if ff == f:
-        return True
-    return False
+    return True
