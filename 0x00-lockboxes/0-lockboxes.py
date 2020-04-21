fc59414c4425c62401e8b14db52fe735ccefe79b
alzheimeer <fogniebla@hotmail.com>
2020-04-20 19:57:12 -0500

    fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,26 +2,18 @@
 def canUnlockAll(boxes):
     '''
     Method that determines if all the boxes can be opened.
-    Args:
-        boxes (lst): List of all the boxes.
-    Returns:
-        True (bool): for success,
-        False (bool): In case of failure.
     '''
     if (len(boxes) == 0):
         return False
     box = list(range(len(boxes)))
     keys = [False]*len(boxes)
     keys[0] = True
-    for k in boxes[0]:
-        if k in box:
-            keys[k] = True
 
-    for c in range(len(boxes)):
-        for i in range(len(boxes)):
+    for c in box:
+        for i in box:
             if keys[i] is True:
                 for k in boxes[i]:
-                    if k in box:
+                    if k in box and keys[k] is False:
                         keys[k] = True
 
     if False in keys:
