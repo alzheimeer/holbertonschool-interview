3af23c781bd03192e22649875dbd83eb859cb693
alzheimeer <fogniebla@hotmail.com>
2020-05-05 11:25:55 -0700

    fix

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,28 +1,24 @@
 #include "binary_trees.h"
 
 /**
-* heap_insert: inserta un valor en un montón binario máximo
+* heap_insert - inserta un valor en un montón binario máximo
 * @root: es un puntero doble al nodo raíz del montón
 * @value: es el valor almacenado en el nodo que se va a insertar
-* Retorno: un puntero al nodo insertado, o NULL en caso de falla
+* Return: un puntero al nodo insertado, o NULL en caso de falla
 */
 
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *nodo;
+	heap_t *nodo;
 
-    nodo = malloc(sizeof(heap_t));
-    if (!nodo)
-        return (NULL);
-    nodo->n = value;
-    if (*root == NULL)
-    {
-        *root = nodo;
+	nodo = malloc(sizeof(heap_t));
+	if (!nodo)
+		return (NULL);
+	nodo->n = value;
+	if (*root == NULL)
+	{
+		*root = nodo;
 		return (nodo);
-    }
-
-
-
-
-    return (nodo);
+	}
+	return (nodo);
 }
