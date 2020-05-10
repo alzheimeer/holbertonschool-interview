a1f4b29013dfc211f979377547bdaa76b1bf0c3f
alzheimeer <fogniebla@hotmail.com>
2020-05-10 00:14:02 -0500

    fix

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -10,18 +10,12 @@ def minOperations(n):
         num: number of operations.
     """
     num = 0
-    if n <= 1 or type(n) != int:
+    if type(n) != int or n <= 1:
         return 0
     while n != 1:
         if n % 2 == 0:
             n = n / 2
             num = num + 2
-        elif n % 3 == 0:
-            n = n / 3
-            num = num + 3
-        elif n % 5 == 0:
-            n = n / 5
-            num = num + 5
         else:
             num = num + n
             n = n / n
