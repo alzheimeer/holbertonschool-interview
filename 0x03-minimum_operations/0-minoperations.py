591d9373d5507fcbdbecdf952746f83e071d871b
alzheimeer <fogniebla@hotmail.com>
2020-05-09 23:03:22 -0500

    feature task 0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+ minOperations
+"""
+
+def minOperations(n):
+    num = 0
+    
+
+    while (n != 1):
+        
+        if (n % 2 == 0):
+            n = n / 2
+            num = num + 2
+        elif (n % 3 == 0):
+            n = n / 3
+            num = num + 3
+        elif (n % 5 == 0):
+            n = n / 5
+            num = num + 5
+        else:
+            num = n
+            n = 1
+        
+    return num
