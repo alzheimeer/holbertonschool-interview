3b4826ec70c9474aff738b53becaf04787eebf03
alzheimeer <fogniebla@hotmail.com>
2020-05-10 01:27:17 -0500

    fix

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 """minOperations"""
-
+ 
 
 def minOperations(n):
-    """This method returns the number of operations to copy paste
+    """number of operations to copy paste
     Args:
         n: given number 
     Returns:
         num: number of operations.
     """
+    p = 2
     num = 0
     if type(n) != int or n <= 1:
         return 0
     while n != 1:
-        if n % 2 == 0:
-            n = n / 2
-            num = num + 2
-        elif n % 3 == 0:
-            n = n / 3
-            num = num + 3
-        elif n % 5 == 0:
-            n = n / 5
-            num = num + 5
+        if n % p == 0:
+            n = n / p
+            num = num + p
         else:
-            num = num + n
-            n = n / n
+            p = p + 1
     return int(num)
