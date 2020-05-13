0d69a54d9dc8c668572d473f547c2778e989151a
alzheimeer <fogniebla@hotmail.com>
2020-05-12 20:03:44 -0700

    fix

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -8,34 +8,34 @@
 */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    size_t nNode;
-    heap_t *nodo;
+	size_t nNode;
+	heap_t *nodo;
 
-    if (*root == NULL)
-        return (*root = binary_tree_node(NULL, value));
-    nNode = get_heap_size(*root) + 1;
-    nodo = insertwithIndex(*root, value, 1, nNode);
-    
-    while (nodo && nodo->parent && nodo->n > nodo->parent->n)
+	if (*root == NULL)
+		return (*root = binary_tree_node(NULL, value));
+	nNode = get_heap_size(*root) + 1;
+	nodo = iwithIndex(*root, value, 1, nNode);
+
+	while (nodo && nodo->parent && nodo->n > nodo->parent->n)
 	{
 		nodo->n = nodo->parent->n;
 		nodo->parent->n = value;
 		nodo = nodo->parent;
 	}
-    return (nodo);
+	return (nodo);
 }
 /**
- * insertwithIdx - Inserts node level ordered
+ * iwithIndex - Inserts node level ordered
  * @root: Type binary_tree pointer of the parent node
  * @value: Type int value of value inserted into node
  * @idx: Type int index of the node
  * @nNode: number the nodes + 1
  * Return: Node inserted
  */
-heap_t *insertwithIndex(binary_tree_t *root, int value, size_t idx, size_t nNode)
+heap_t *iwithIndex(binary_tree_t *root, int value, size_t idx, size_t nNode)
 {
 	heap_t *nodel, *noder;
-    size_t parent = nNode / 2;
+	size_t parent = nNode / 2;
 
 	if (!root)
 		return (NULL);
@@ -44,14 +44,14 @@ heap_t *insertwithIndex(binary_tree_t *root, int value, size_t idx, size_t nNode
 		if (nNode % 2)
 			return (root->right = binary_tree_node(root, value));
 		else
-    		return (root->left = binary_tree_node(root, value));
+			return (root->left = binary_tree_node(root, value));
 	}
-	nodel = insertwithIndex(root->left, value, 2 * idx , nNode);
-    noder = insertwithIndex(root->right, value, 2 * idx + 1, nNode);
+	nodel = iwithIndex(root->left, value, 2 * idx, nNode);
+	noder = iwithIndex(root->right, value, 2 * idx + 1, nNode);
 	if (nodel)
 		return (nodel);
 	else
-        return (noder);
+		return (noder);
 }
 /**
  * get_heap_size - gets number of nodes in heap
@@ -60,10 +60,11 @@ heap_t *insertwithIndex(binary_tree_t *root, int value, size_t idx, size_t nNode
  */
 size_t get_heap_size(heap_t *root)
 {
-    int left = 0, right = 0;
+	int left = 0, right = 0;
+
 	if (!root)
 		return (0);
-    left = get_heap_size(root->left);
-    right = get_heap_size(root->right);
+	left = get_heap_size(root->left);
+	right = get_heap_size(root->right);
 	return (1 + left + right);
 }
