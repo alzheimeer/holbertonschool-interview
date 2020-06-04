ff5c00bd35423d13037595364477c66cc93395af
alzheimeer <fogniebla@hotmail.com>
2020-06-04 11:26:03 -0700

    empty files

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,19 @@
+!#/bin/bash
+#include "list.h"
+
+   /**
+    *check_cycle - checks if a singly linked list has a cycle in it.
+    *@list: list
+    *Only these functions are allowed: write, printf, putchar, puts,
+    *malloc, free
+    *Return: 0 if there is no cycle, 1 if there is a cycle
+    */
+int check_cycle(listint_t *list)
+{
+
+
+
+
+
+
+}
