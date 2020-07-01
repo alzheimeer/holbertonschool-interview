a7d5a6e34a58da0a67ebf2a6f5fa10943240abe7
Edgar <fogniebla@hotmail.com>
2020-07-01 01:25:43 -0500

    new  task

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "slide_line.h"
+
+/**
+ * slide_line - Prints out an array of integer, followed by a new line
+ * 
+ * @line: points to an array of integers containing
+ * @size: elements, that must be slided & merged to the direction represented by direction
+ * @direction: can be either: SLIDE_LEFT  - SLIDE_RIGHT
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef _H_
+#define _H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void print_array(int const *array, size_t size);
+int slide_line(int *line, size_t size, int direction);
+
+#endif
