446239523415c6b0b0adb8cfc6eb3cf395bfac56
alzheimeer <fogniebla@hotmail.com>
2020-07-08 21:12:54 -0700

    fix

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -1,15 +1,14 @@
-#ifndef _H_
-#define _H_
-#include <stddef.h>
-#include <stdio.h>
+#ifndef _SLIDE_LINE_H
+#define _SLIDE_LINE_H
+
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
 
-
-void menger(int level);
-
-
-
+void left_order(int *line, size_t size);
+void reverse_array(int *a, int n);
+int slide_line(int *line, size_t size, int direction);
 
 #endif
