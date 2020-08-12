0404412a5c62ddea13d12d73b72504f458948ab4
alzheimeer <fogniebla@hotmail.com>
2020-08-12 13:58:31 -0700

    fix

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -1,5 +1,10 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -9,15 +14,21 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
-{
-        int n;
-        struct binary_tree_s *parent;
-        struct binary_tree_s *left;
-        struct binary_tree_s *right;
-};
 
-typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+typedef struct binary_tree_s avl_t;
+
+void binary_tree_print(const binary_tree_t *tree);
+
+avl_t *new_node();
+avl_t *sort_data(size_t left, size_t right, int *arr, avl_t *tree);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 
-#endif /* LISTS_H */
+#endif /* _BINARY_TREES_H_ */
