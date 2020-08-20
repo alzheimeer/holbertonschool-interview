0748fb3668dac38fbb342d925018db3025065af0
alzheimeer <fogniebla@hotmail.com>
2020-08-19 18:19:23 -0700

    feature task old

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `https://swapi.co/api/films/${process.argv[2]}/`;
-
-request(url, function (error, response, body) {
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
-    for (const actor of characters) {
-      request(actor, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
+    for (const actor in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[actor], (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
       });
+      console.log(res);
     }
   }
 });
