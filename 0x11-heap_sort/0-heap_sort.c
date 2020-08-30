e4e2e93044419beaab8eba1378b44d7f8633dd25
alzheimeer <fogniebla@hotmail.com>
2020-08-30 07:34:30 -0700

    empty files

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+void heap_sort(int *array, size_t size)
+{
+
+	if(!array || size < 1)
+		return(0);
+
+	
+
+
+
+}
---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,9 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+
+#endif
