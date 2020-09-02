853f986793af7fb560f3943313dd893f6fb371a6
alzheimeer <fogniebla@hotmail.com>
2020-09-01 23:42:27 -0700

    feature heap

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -4,18 +4,51 @@
 
 
 /**
- * main - Entry point
- *
+ * heap_sort - Heap Sort
+ * Heap properties:
+ * Left child of array[i] is array[2i + 1] (given 2i + 1 < array.size)
+ * Right child of array[i] is array[2i + 2] (given 2i + 2 < array.size)
+ * parent of array[i] is array[(i-1)/2]
+ * @array: array of numbers
+ * @size: Size of the array
  * Return: Always 0
  */
 void heap_sort(int *array, size_t size)
 {
+	size_t len = size, root, temp;
+	ssize_t i_ult, padre;
+	int tempA;
 
-	if(!array || size < 1)
-		return(0);
-
-	
-
-
-
+	if (size < 2)
+		return;
+	while (len)
+	{
+		i_ult = size - 1;
+		padre = (i_ult - 1) / 2;
+		while (padre >= 0)
+		{
+			padre--;
+			root = padre;
+			while ((2 * root + 1) < len)
+			{
+				temp = root;
+				if (array[(2 * root + 1)] > array[root])
+					temp = (2 * root + 1);
+				if (2 * root + 2 < len && array[2 * root + 2] > array[temp])
+					temp = 2 * root + 2;
+				if (temp == root)
+					break;
+				tempA = array[root];
+				array[root] = array[temp];
+				array[temp] = tempA;
+				print_array(array, size);
+				root = temp;
+			}
+		}
+		tempA = array[0];
+		array[0] = array[len - 1];
+		array[len - 1] = tempA;
+		print_array(array, size);
+		len--;
+	}
 }
--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -3,7 +3,12 @@
 
 
 
+
+
+
+
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
+
 
 #endif
