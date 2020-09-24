334ce697267b46cb4a91e5e285f00e8d22dd1307
alzheimeer <fogniebla@hotmail.com>
2020-09-23 21:58:06 -0700

    fix

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -16,7 +16,6 @@ int heap_extract(heap_t **root)
 	a = (*root)->n;
 	(*root)->n = 0;
 
-
 	if ((*root)->left->n > (*root)->right->n)
 	{
 		(*root)->n = (*root)->left->n;
@@ -40,35 +39,29 @@ int heap_extract(heap_t **root)
 
 int reorden(heap_t **root)
 {
-	heap_t *x = *root;
+	heap_t *x = (*root)->parent;
 
 	if (!(*root)->left && !(*root)->right)
 	{
 		if ((*root)->parent->left->n == (*root)->n)
-		{
-			x = x->parent, x->left = NULL, free(*root);
-			verify(&x);
-		}
+			(*root)->parent->left = NULL;
 		else if ((*root)->parent->right->n == (*root)->n)
-		{
-			x = x->parent, x->right = NULL, free(*root);
-			verify(&x);
-		}
+			(*root)->parent->right = NULL;
+		free(*root);
+		verify(&x);
 		return (0);
 	}
 	if (!(*root)->left && (*root)->right)
 	{
 		(*root)->n = (*root)->right->n, (*root)->right->n = 0;
 		reorden(&(*root)->right);
-		return (0);
 	}
 	else if (!(*root)->right && (*root)->left)
 	{
 		(*root)->n = (*root)->left->n, (*root)->left->n = 0;
 		reorden(&(*root)->left);
-		return (0);
 	}
-	if ((*root)->left->n > (*root)->right->n)
+	else if ((*root)->left->n > (*root)->right->n)
 	{
 		(*root)->n = (*root)->left->n, (*root)->left->n = 0;
 		reorden(&(*root)->left);
@@ -88,35 +81,33 @@ int reorden(heap_t **root)
  */
 void verify(heap_t **x)
 {
-	heap_t *z = (*x)->parent;
-
 	if ((*x)->left && !(*x)->right)
 	{
-		if (z->right->right)
+		if ((*x)->parent->right->right)
 		{
-			(*x)->right = z->right->right;
-			z->right->right = NULL;
+			(*x)->right = (*x)->parent->right->right;
+			(*x)->parent->right->right = NULL;
 			(*x)->right->parent = (*x);
 		}
-		else if (z->right->left)
+		else if ((*x)->parent->right->left)
 		{
-			(*x)->right = z->right->left;
-			z->right->left = NULL;
+			(*x)->right = (*x)->parent->right->left;
+			(*x)->parent->right->left = NULL;
 			(*x)->right->parent = (*x);
 		}
 	}
 	else if (!(*x)->left && (*x)->right)
 	{
-		if (z->right->right)
+		if ((*x)->parent->right->right)
 		{
-			(*x)->left = z->right->right;
-			z->right->right = NULL;
+			(*x)->left = (*x)->parent->right->right;
+			(*x)->parent->right->right = NULL;
 			(*x)->left->parent = (*x);
 		}
-		else if (z->right->left)
+		else if ((*x)->parent->right->left)
 		{
-			(*x)->left = z->right->left;
-			z->right->left = NULL;
+			(*x)->left = (*x)->parent->right->left;
+			(*x)->parent->right->left = NULL;
 			(*x)->left->parent = (*x);
 		}
 	}
