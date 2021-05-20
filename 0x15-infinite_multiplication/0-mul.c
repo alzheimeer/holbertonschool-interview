412e934ced52036b6ae9caf66d5c7c5d4cf7e83d
Edgar Carlos M. Quintero <fogniebla@hotmail.com>
2021-05-20 16:35:32 -0500

    fix

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,42 +1,6 @@
-#include "holberton.h"
-
-/**
- * _isdigit - check if a string is a digit
- * Description - This function is to check if a string is a digit
- * @n: Sring to guess if is an uppercase character
- * Return: 1 if n is a digit, 0 otherwise
- */
-int _isdigit(char *n)
-{
-	int i;
-
-	i = 0;
-	while (*(n + i) != '\0')
-	{
-		if (*(n + i) < '0' || *(n + i) > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - Return the len of a string
- * Description: This function shows the length of a given string
- * @s: Pointer that contains the string
- * Return: @s len
- */
-int _strlen(const char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * is_error - error checker
@@ -48,16 +12,25 @@ int _strlen(const char *s)
 
 int is_error(int argc, char **argv)
 {
-	if (argc != 3 || !(_isdigit(argv[1])) || !(_isdigit(argv[2])))
+	int i, j;
+
+	if (argc != 3)
 	{
-		puts("Error");
+		printf("Error\n");
 		exit(98);
 	}
-	if (argv[1][0] == '0' || argv[2][0] == '0')
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-		return (0);
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
 	}
+	return (0);
 }
 
 /**
@@ -74,6 +47,12 @@ int main(int argc, char **argv)
 	int *result;
 
 	is_error(argc, argv);
+	len1 = strlen(argv[1]), len2 = strlen(argv[2]);
+	if (argv[1][0] == '0' || argv[2][0] == '0')
+	{
+		printf("0\n");
+		return (EXIT_SUCCESS);
+	}
 	result = calloc(len1 + len2, sizeof(*result));
 	if (!result)
 		return (EXIT_FAILURE);
